8ee879ae3f72c7b1cf6bcd7dfd7a5b2e461ff2a1
Azeez Samad <seshp@ymail.com>
2022-04-23 18:58:58 +0100

    add component for super

---
+++ src/components/Power/Power.js
@@ -0,0 +1,18 @@
+import './PowerStyles.css'
+
+const Power = () => {
+  return (
+    <div className="super">
+        <div className="container">
+             <h1 className="top">Super</h1>
+        <div className="bottom">
+            <button className="btn btn-dark">Drive</button>
+            <button className="btn">Ride</button>
+        </div>
+        </div>
+      
+    </div>
+  );
+}
+
+export default Power;
